3eddc5ff0f13f4e534be8801a0000322d2b480cb
alfonsof <alfonsofb@gmail.com>
2018-09-28 17:34:03 +0200

    First commit

---
+++ gcloudcomputeengine/README.md
@@ -0,0 +1,82 @@
+# Google Cloud Compute Engine VM instances Python example
+
+This folder contains a Python application example that handles Compute Engine VM instances on Google Cloud Platform (GCP).
+
+## Requirements
+
+* You must have a [Google Cloud Platform (GCP)](http://cloud.google.com/) account.
+
+* You can install and use the Google Cloud SDK.
+
+  The Google Cloud SDK is a set of tools for Google Cloud Platform.
+  It contains gcloud, gsutil, and bq, which you can use to access Google Compute Engine, Google Cloud Storage, Google BigQuery,
+  and other products and services from the command line. You can run these tools interactively or in your automated scripts.* The code was written for Python 3 and Google Cloud Python Client Library.
+
+* The code was written for Python 3 and Google API Python Client Library.
+
+* Install the Google API Python Client Library.
+
+  Install the latest Google API Python Client Library release via pip:
+
+  ```bash
+  pip install google-api-python-client
+  ```
+
+## Using the code
+
+* Configure your Google Cloud access keys.
+
+  The Google Cloud client library for Java allows you to use several authentication schemes.
+
+  The application uses Application Default Credentials through a JSON service account key for authenticating.
+
+  The credentials are taken from `GOOGLE_APPLICATION_CREDENTIALS` environment variable.
+
+  For example:
+
+  ```bash
+  GOOGLE_APPLICATION_CREDENTIALS = /path/to/my/key.json
+  ```
+
+  Use the [Google Cloud Platform console](http://cloud.google.com/):
+
+  * Go to the Google Cloud Project.
+
+  * Prepare the credentials:
+    * Create a Service account.
+
+      For example:
+
+      ```bash
+      Name: gcloud-java-examples
+      Role: Owner
+      Email: gcloud-java-examples@gcloud-java-examples.iam.gserviceaccount.com
+      ```
+
+    * Create a key as a JSON file and download it.
+
+    * Add the Service accounts id (Ex.: gcloud-java-examples@gcloud-java-examples.iam.gserviceaccount.com) as a member of the project in the IAM.
+
+  * Set the GOOGLE_APPLICATION_CREDENTIALS environment variable in your Operating System with the path of your JSON service account key file.
+
+* Run the code.
+
+  Run application:
+
+  ```bash
+  python computeengineinstances.py
+  ```
+
+  You can select an option in the menu in order to run every command:
+
+  * 1 = List all VM instances
+  * 2 = Create VM instance
+  * 3 = List VM instance
+  * 4 = Start VM instance
+  * 5 = Stop VM instance
+  * 6 = Reset instance
+  * 7 = Delete instance
+
+* Test the application.
+
+  You should see the new VM instance and modification of states with the Google Cloud console.
---
+++ gcloudcomputeengine/computeenginehelper.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# computeenginehelper.py
+# It has methods for managing Google Cloud Compute Engine VM instances.
+
+import sys
+import googleapiclient.discovery
+
+ZONE_NAME           = 'us-east1-b'                      # Zone name
+PROJECT_NAME        = 'gcloud-java-examples'            # Project name
+IMAGE_NAME          = "ubuntu-1604-lts"                 # Image name
+IMAGE_PROJECT_NAME  = "ubuntu-os-cloud"                 # Image project name
+INSTANCE_TYPE       = "n1-standard-1"                   # Instance type
+INSTANCE_NAME       = "my-instance"                     # Name of the instance
+
+
+def list_instances():
+  """
+  List all Compute Engine VM instances associated with an Google Cloud account
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Listing VM instances ...')
+
+  # Describe instances
+  response = compute.instances().list(project=PROJECT_NAME, zone=ZONE_NAME).execute()
+  print('Instances in project "%s" and zone "%s":' % (PROJECT_NAME, ZONE_NAME))
+  if (response.get('items')):
+    for instance in response['items']:
+      print(' - Id:           ' + instance['id'])
+      print('   Name:         ' + instance['name'])
+      print('   Status:       ' + instance['status'])
+      print('   Machine type: ' + instance['machineType'])
+  else:
+    print('NO instances')
+
+  return
+
+
+def create_instance():
+  """
+  Create a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Creating VM instance ...')
+
+  # Get the latest image
+  image_response = compute.images().getFromFamily(
+                             project=IMAGE_PROJECT_NAME, family=IMAGE_NAME).execute()
+  source_disk_image = image_response['selfLink']
+
+  # Configure the machine
+  #machine_type = 'zones/' + ZONE_NAME + '/machineTypes/' + INSTANCE_TYPE
+  machine_type = 'zones/%s/machineTypes/%s' % (ZONE_NAME, INSTANCE_TYPE)
+
+  config = {
+        'name': INSTANCE_NAME,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }]
+  }
+
+  response = compute.instances().insert(project=PROJECT_NAME,
+                            zone=ZONE_NAME,
+                            body=config).execute()
+
+  print('Instance Id:   ' + response['id'])
+  print('Response:   ' + response['name'])
+
+  return response['id']
+
+
+def list_instance(instance_id):
+  """
+  List a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Listing VM instance ...')
+  print('Instance Id: ' + instance_id)
+
+  # List the VM instance
+  response = compute.instances().get(project=PROJECT_NAME, zone=ZONE_NAME, instance=INSTANCE_NAME).execute()
+  print(response)
+
+  print(' - Id:           ' + response['id'])
+  print('   Name:         ' + response['name'])
+  print('   Status:       ' + response['status'])
+  print('   Machine type: ' + response['machineType'])
+
+  return
+
+
+def start_instance(instance_id):
+  """
+  Start a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Starting VM instance ...')
+
+  # Start VM instance
+  compute.instances().start(
+        project=PROJECT_NAME,
+        zone=ZONE_NAME,
+        instance=INSTANCE_NAME).execute()
+
+  return
+
+
+def stop_instance(instance_id):
+  """
+  Stop a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Stopping VM instance ...')
+
+  # Stop VM instance
+  compute.instances().stop(
+        project=PROJECT_NAME,
+        zone=ZONE_NAME,
+        instance=INSTANCE_NAME).execute()
+
+  return
+
+
+def reset_instance(instance_id):
+  """
+  Reset a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Resetting VM instance ...')
+
+  # Reset VM instance
+  compute.instances().reset(
+        project=PROJECT_NAME,
+        zone=ZONE_NAME,
+        instance=INSTANCE_NAME).execute()
+
+  return
+
+
+def delete_instance(instance_id):
+  """
+  Delete a Compute Engine VM instance
+  """
+  # Build and initialize the API
+  compute = googleapiclient.discovery.build('compute', 'v1')
+
+  print('Deleting VM instance ...')
+
+  # Delete VM instance
+  compute.instances().delete(
+        project=PROJECT_NAME,
+        zone=ZONE_NAME,
+        instance=INSTANCE_NAME).execute()
+
+  return
