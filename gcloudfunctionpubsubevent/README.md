903fa48921ef3ad33c36611dbc75330a7d2c919e
alfonsof <alfonsofb@gmail.com>
2018-10-08 12:22:26 +0200

    Updated comments and identation

--- gcloudfunctionpubsubevent/README.md
+++ gcloudfunctionpubsubevent/README.md
@@ -8,7 +8,7 @@ It handles a Google Cloud Function that sends information about a Cloud Pub/Sub 
 
 * You must have a [Google Cloud Platform (GCP)](http://cloud.google.com/) account.
 
-* The code was written for Python 3 and Google Cloud Python Client Library.
+* The code was written for Python 3.
 
 ## Using the code
 
--- gcloudfunctionpubsubevent/main.py
+++ gcloudfunctionpubsubevent/main.py
@@ -4,17 +4,17 @@
 # It handles a Google Cloud Function that sends information about a Cloud Pub/Sub event that depends on the input to the function log.
 
 def pubsub_event(event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
-        Args:
-            event (dict): Event payload.
-            context (google.cloud.functions.Context): Metadata for the event.
-    """
-    
-    import base64
-    
-    if 'data' in event:
-        name = base64.b64decode(event['data']).decode('utf-8')
-    else:
-        name = 'World'
-    print('Hello {}!'.format(name))
+  """Triggered from a message on a Cloud Pub/Sub topic.
+      Args:
+          event (dict): Event payload.
+          context (google.cloud.functions.Context): Metadata for the event.
+  """
+
+  import base64
+
+  if 'data' in event:
+    name = base64.b64decode(event['data']).decode('utf-8')
+  else:
+    name = 'World'
+  print('Hello {}!'.format(name))
     
