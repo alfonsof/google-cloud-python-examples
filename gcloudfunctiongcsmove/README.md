eb15c563906fac00325f9123246f3271ea3ada48
alfonsof <alfonsofb@gmail.com>
2018-10-08 12:18:39 +0200

    First commit

---
+++ gcloudfunctiongcsmove/README.md
@@ -0,0 +1,45 @@
+# Google Cloud Function Cloud Storage Move Python example
+
+This folder contains a Google Cloud Function example in Python on Google Cloud Platform (GCP).
+
+It handles a Google Cloud Function that Moves an object when it appears in a Cloud Storage bucket to another Cloud Storage bucket.
+
+## Requirements
+
+* You must have a [Google Cloud Platform (GCP)](http://cloud.google.com/) account.
+
+* The code was written for Python 3 and Google Cloud Python Client Library.
+
+## Using the code
+
+* Access the Google Cloud console.
+
+* Create a Cloud Storage bucket for the source and another Cloud Storage bucket for the target.
+
+* Create a Google Cloud Function:
+  * Name: `<CLOUD_FUNCTION_NAME>`
+  * Memory allocated: `256 MB`
+  * Trigger: `Cloud Storage`
+  * Event Type: `Finalise/Create`
+  * Bucket: `<BUCKET_NAME>`
+  * Source code. You can use 2 options:
+    * Inline editor:
+      Edit the code of the `main.py` and `requirements.txt` files in the browser.
+    * ZIP upload:
+      Upload a ZIP file containing the `main.py` and `requirements.txt` files.
+      * ZIP file: `<ZIP_LOCAL_NAME>`
+      * Stage bucket: `<BUCKET_NAME_FOR_STAGGING>`
+  * Runtime: `Python 3.7 (Beta)`
+  * Function to execute: `gcs_move`
+  * Region: `<GOOGLE_CLOUD_REGION>`
+  * Timeout: `60 seconds`
+
+* Save the Google Cloud Function.
+
+  The function is deployed and run.
+
+* Test the function.
+
+  Copy a file in the source Cloud Storage bucket.
+
+  The object from the source Cloud Storage bucket should be copied to the target Cloud Storage bucket and deleted in the source Cloud Storage bucket.
---
+++ gcloudfunctiongcsmove/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# main.py
+# It handles a Google Cloud Function that moves an object when it appears in a Cloud Storage bucket to another Cloud Storage bucket.
+
+from google.cloud import storage
+from google.cloud.exceptions import NotFound
+from google.cloud.exceptions import Forbidden
+
+def gcs_move(event, context):
+  """Triggered by a change to a Cloud Storage bucket.
+  Args:
+          event (dict): Event payload.
+          context (google.cloud.functions.Context): Metadata for the event.
+  """
+
+  DESTINATION_BUCKET = 'destinationbucket'   # Destination Bucket name
+
+  file = event
+
+  source_bucket_name = file['bucket']
+  source_blob_name = file['name']
+  destination_bucket_name = DESTINATION_BUCKET
+  destination_blob_name = source_blob_name
+
+  print('From - bucket:', source_bucket_name)
+  print('From - object:', source_blob_name)
+  print('To - bucket:  ', destination_bucket_name)
+  print('To - object:  ', destination_blob_name)
+
+  print('Moving object ...')
+  
+  # Instantiate the client.
+  client = storage.Client()
+
+  try:
+    # Get the source bucket.
+    source_bucket = client.get_bucket(source_bucket_name)
+    # Instantiate the source object.
+    source_blob = source_bucket.blob(source_blob_name)
+    # Get the destination bucket.
+    destination_bucket = client.get_bucket(destination_bucket_name)
+    # Copies a blob from one bucket to another one.
+    source_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
+    # Delete the source object.
+    source_blob.delete()
+    print('\nMoved')
+  except NotFound:
+    print('Error: Bucket/Blob does NOT exists!!')
+    pass
+  except Forbidden:
+    print('Error: Forbidden, you do not have access to it!!')
+    pass
